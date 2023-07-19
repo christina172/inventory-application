baf57a36a0093d6150da16df7424f6981088da0e
christina172 <117263200+christina172@users.noreply.github.com>
2023-07-19 13:09:09 +0300

    Implement add, update and delete pages with forms

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const Category = require("../models/category");
 const Item = require("../models/item");
 
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
@@ -49,30 +50,154 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 
 // Display Category add form on GET.
 exports.category_add_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category add GET");
+    res.render("category_form", { title: "Add Category" });
 });
 
 // Handle Category add on POST.
-exports.category_add_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category add POST");
-});
+exports.category_add_post = [
+    // Validate and sanitize the name field.
+    body("name", "Category name must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Description must be between 1 and 250 characters long.")
+        .trim()
+        .isLength({ min: 1, max: 250 })
+        .escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a genre object with escaped and trimmed data.
+        const category = new Category({
+            name: req.body.name,
+            description: req.body.description
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            res.render("category_form", {
+                title: "Add Category",
+                category: category,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            // Data from form is valid.
+            // Check if Category with same name already exists.
+            const categoryExists = await Category.findOne({ name: req.body.name }).exec();
+            if (categoryExists) {
+                // Category exists, redirect to its detail page.
+                res.redirect(categoryExists.url);
+            } else {
+                await category.save();
+                // New genre saved. Redirect to genre detail page.
+                res.redirect(category.url);
+            }
+        }
+    }),
+];
 
 // Display Category delete form on GET.
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category delete GET");
+    // Get details of category and all items of this category
+    const [category, allItemsInCategory] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({ category: req.params.id }, "brand name model").exec(),
+    ]);
+
+    if (category === null) {
+        // No results.
+        res.redirect("/music/categories");
+    }
+
+    res.render("category_delete", {
+        title: "Delete Category",
+        category: category,
+        category_items: allItemsInCategory,
+    });
 });
 
 // Handle Category delete on POST.
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category delete POST");
+    // Get details of category and all items of this category
+    const [category, allItemsInCategory] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({ category: req.params.id }, "brand name model").exec(),
+    ]);
+
+    if (allItemsInCategory.length > 0) {
+        // Category has items. Render in same way as for GET route.
+        res.render("author_delete", {
+            title: "Delete Author",
+            category: category,
+            category_items: allItemsInCategory,
+        });
+        return;
+    } else {
+        // Category has no items. Delete object and redirect to the list of categories.
+        await Category.findByIdAndRemove(req.body.categoryid);
+        res.redirect("/music/categories");
+    }
 });
 
 // Display Category update form on GET.
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category update GET");
+    // Get category
+    const category = await Category.findById(req.params.id).exec();
+
+    if (category === null) {
+        // No results.
+        const err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("category_form", {
+        title: "Update Category",
+        category: category,
+    });
 });
 
 // Handle Category update on POST.
-exports.category_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category update POST");
-});
+exports.category_update_post = [
+    // Validate and sanitize the name field.
+    body("name", "Category name must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Description must be between 1 and 250 characters long.")
+        .trim()
+        .isLength({ min: 1, max: 250 })
+        .escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a Category object with escaped/trimmed data and old id.
+        const category = new Category({
+            name: req.body.name,
+            description: req.body.description,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            res.render("category_form", {
+                title: "Add Category",
+                category: category,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            // Data from form is valid. Update the record.
+            const thecategory = await Category.findByIdAndUpdate(req.params.id, category, {});
+            // Redirect to category detail page.
+            res.redirect(thecategory.url);
+        }
+    }),
+];
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,7 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
 
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Items.
@@ -33,30 +34,219 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 
 // Display Item add form on GET.
 exports.item_add_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item add GET");
+    // Get all categories, which we can use to add new item.
+    const allCategories = await Category.find().exec();
+
+    res.render("item_form", {
+        title: "Add Item",
+        categories: allCategories,
+    });
 });
 
 // Handle Item add on POST.
-exports.item_add_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item add POST");
-});
+exports.item_add_post = [
+    // Validate and sanitize the name field.
+    body("name", "Item name must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("model", "Item model must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("brand", "Item brand must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("category", "Item category must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Item description must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("details", "Item details must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("materials", "Item materials must be between 1 and 300 characters long.")
+        .trim()
+        .isLength({ min: 1, max: 300 })
+        .escape(),
+    body("accessories", "Item accessories must be between 1 and 300 characters long.")
+        .trim()
+        .isLength({ min: 1, max: 300 })
+        .escape(),
+    body("price", "Item price must be an integer and must be greater than 1 $ and smaller than 100 000 $.")
+        .trim()
+        .isInt({ min: 1, max: 100000 })
+        .escape(),
+    body("number_in_stock", "Item quantity (number in stock) must be an integer.")
+        .trim()
+        .isInt()
+        .escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create an item object with escaped and trimmed data.
+        const item = new Item({
+            name: req.body.name,
+            model: req.body.model,
+            brand: req.body.brand,
+            category: req.body.category,
+            description: req.body.description,
+            details: req.body.details,
+            materials: req.body.materials,
+            accessories: req.body.accessories,
+            price: req.body.price,
+            number_in_stock: req.body.number_in_stock
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            const allCategories = await Category.find().exec();
+
+            res.render("item_form", {
+                title: "Add Item",
+                item: item,
+                categories: allCategories,
+                errors: errors.array(),
+            });
+        } else {
+            // Data from form is valid. Save item.
+            await item.save();
+            res.redirect(item.url);
+        }
+    }),
+];
 
 // Display Item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item delete GET");
+    // Get details of item
+    const item = await Item.findById(req.params.id).exec();
+
+    if (item === null) {
+        // No results.
+        res.redirect("/music/items");
+    }
+
+    res.render("item_delete", {
+        title: "Delete Item",
+        item: item,
+    });
 });
 
 // Handle Item delete on POST.
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item delete POST");
+    await Item.findByIdAndRemove(req.body.itemid);
+    res.redirect("/music/items");
 });
 
 // Display Item update form on GET.
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update GET");
+    // Get item
+    const [item, allCategories] = await Promise.all([
+        Item.findById(req.params.id).exec(),
+        Category.find().exec(),
+    ]);
+
+    if (item === null) {
+        // No results.
+        const err = new Error("Item not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("item_form", {
+        title: "Update Item",
+        item: item,
+        categories: allCategories,
+    });
 });
 
 // Handle Item update on POST.
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update POST");
-});
+exports.item_update_post = [
+    // Validate and sanitize the name field.
+    body("name", "Item name must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("model", "Item model must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("brand", "Item brand must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("category", "Item category must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Item description must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("details", "Item details must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("materials", "Item materials must be between 1 and 300 characters long.")
+        .trim()
+        .isLength({ min: 1, max: 300 })
+        .escape(),
+    body("accessories", "Item accessories must be between 1 and 300 characters long.")
+        .trim()
+        .isLength({ min: 1, max: 300 })
+        .escape(),
+    body("price", "Item price must be an integer and must be greater than 1 $ and smaller than 100 000 $.")
+        .trim()
+        .isInt({ min: 1, max: 100000 })
+        .escape(),
+    body("number_in_stock", "Item quantity (number in stock) must be an integer.")
+        .trim()
+        .isInt()
+        .escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create an Item object with escaped/trimmed data and old id.
+        const item = new Item({
+            name: req.body.name,
+            model: req.body.model,
+            brand: req.body.brand,
+            category: req.body.category,
+            description: req.body.description,
+            details: req.body.details,
+            materials: req.body.materials,
+            accessories: req.body.accessories,
+            price: req.body.price,
+            number_in_stock: req.body.number_in_stock,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            const allCategories = await Category.find().exec();
+
+            res.render("item_form", {
+                title: "Add Item",
+                item: item,
+                categories: allCategories,
+                errors: errors.array(),
+            });
+        } else {
+            // Data from form is valid. Update the record.
+            const theitem = await Item.findByIdAndUpdate(req.params.id, item, {});
+            // Redirect to item detail page.
+            res.redirect(theitem.url);
+        }
+    }),
+];
